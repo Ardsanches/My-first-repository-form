68a8267c783c5740168eda3aaee4574f1914669a
Ardsanches <anradisa@gmail.com>
2025-04-07 12:46:36 +0100

    Update ApplicationDbContext.cs

--- WebApplication2/WebApplication2/Data/ApplicationDbContext.cs
+++ WebApplication2/WebApplication2/Data/ApplicationDbContext.cs
@@ -1,6 +1,10 @@
 using Microsoft.EntityFrameworkCore;
 using WebApplication2.Models;
 
+/* ApplicationDbContext is the class that manages the database connection and CRUD operations for FormData
+It inherits from DbContext provided by Entity Framework to interact with the database
+The DbSet<FormData> property represents the FormData table in the database */
+
 namespace WebApplication2.Data
 {
     public class ApplicationDbContext : DbContext
